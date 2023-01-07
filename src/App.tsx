fb3dccefc3e0a3e064e1d0e36cf254682992c235
Abdulla <94592483+Prouzad@users.noreply.github.com>
2023-01-07 17:51:41 +0500

    feat: add carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
+import Carousels from './components/Carousel/Carousel';
 import Header from './components/Header/Header';
+import ImageArray from './components/PhotoImport'
+
 
 function App() {
+	console.log(ImageArray)
   return (
     <div className="App">
-     <Header/>
+     	<Header/>
+			<div className='bodyBox'>
+				<Carousels items={ImageArray}/>
+			</div>
     </div>
   );
 }
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,8 +1,22 @@
 import style from './Carousel.module.css'
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-function Carousel() {
+const responsive = {
+	0: {
+		items: 5,
+	},
+	1024: {
+			items: 3,
+			itemsFit: 'contain',
+	}
+}
+
+
+function Carousel(props: {items: JSX.Element[]}) {
   return (
     <div className={style.carousel}>
+			<AliceCarousel disableDotsControls items={props.items} />
     </div>
   );
 }
