13d32df8eb1a075d98440cb858ba6672c517993b
prouzad <doston.ergashev.97@mail.ru>
2023-01-09 22:51:14 +0500

    feat: Add select

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -8,6 +8,8 @@ import "slick-carousel/slick/slick-theme.css";
 import MyButton from '../Button/Button';
 import Data from '../Data';
 import Cards from '../Cards/Card';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { grey } from '@mui/material/colors';
 
 const category = [
 	'Избранные',
@@ -46,7 +48,21 @@ function Main() {
 							)
 						})}
 					</div>
-					<MyButton title={`Eщё ${"\u2304"}`}/>
+					<FormControl sx={{ m: 1, minWidth: 120, background: '#ebebf1', border: 'none', borderRadius: '14px' }} size="small">
+						<InputLabel id="demo-simple-select-label" sx={{color: 'black' }}>Ещё</InputLabel>
+						<Select
+							labelId="demo-simple-select-label"
+							id="demo-simple-select"
+							label="Ещё"
+							sx={{boxShadow: 'none', '.MuiOutlinedInput-notchedOutline': { border: 0 }}}
+						>
+						<div className={style.selec}>
+							{category.map((item, idx)=>{
+								return <MenuItem sx={{width: '256px', padding: '15px'}} value={item} key={idx}>{item}</MenuItem>
+							})}
+						 </div>
+						</Select>
+      </FormControl>
 				</div>
 				<div className={style.cardsBox}>
 					{Data.map((item, idx)=>{
